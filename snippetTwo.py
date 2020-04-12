54248d2166e7721915655028df1b75dfe45bb8d8
Adib Ahsan Chowdhury <adibahsan@iut-dhaka.edu>
2020-04-12 22:11:24 +0600

    styles added

--- snippetTwo.py
+++ snippetTwo.py
@@ -3,6 +3,9 @@ from matplotlib import pyplot as plt
 dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
 
+plt.style.use("fivethirtyeight")
+# plt.style.use("ggplot")
+plt.xkcd()
 plt.plot(dev_x, dev_y)
 
 py_dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
